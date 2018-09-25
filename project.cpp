f58d4ed5fd8c215c420cf1a9db815a284f0b6dfe
JeremyFongSP <jeremy.fongsanpin@gmail.com>
2018-09-24 20:01:41 -0400

    Initial cpp commit

---
+++ project.cpp
@@ -0,0 +1,14 @@
+
+#include <iostream>
+#include <cstdio>
+#include <cmath>
+
+using namespace std;
+
+//Put all of the above into a header
+
+
+int main(){
+	cout << "\nHello World!";
+	return 0;
+}
