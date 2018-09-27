6c0aea1a2585a2a1879265aaf9b430b7ba669bc8
Jeremy <jeremy.fongsanpin@gmail.com>
2018-09-27 18:00:46 -0400

    Add Visual Studio Files

---
+++ Final_Project/Final_Project/Final_Project.cpp
@@ -0,0 +1,16 @@
+
+/*****************************************
+This MECH 415 project was made by:
+Annie Fong
+Jeremy Fong San Pin
+Vivek Patel
+*****************************************/
+
+#include "Final_Project.h"
+
+int main(){
+	cout << "\nHello World!";
+	cout << "\nHello World !!!";
+	cout << "\nHello World !!!!!!";
+	return 0;
+}
---
+++ Final_Project/Final_Project/Final_Project.h
@@ -0,0 +1,14 @@
+
+/*****************************************
+This MECH 415 project was made by:
+Annie Fong
+Jeremy Fong San Pin
+Vivek Patel
+*****************************************/
+
+#include <iostream>
+#include <cstdio>
+#include <cmath>
+
+using namespace std;
+
